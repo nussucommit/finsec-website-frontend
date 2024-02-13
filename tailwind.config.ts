7c70de41617f79c7eb09ffd29c9e74de669fe60f
mihirniyogi <E1122052@u.nus.edu>
2024-02-13 15:35:37 +0800

    Added colors, nussu logo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        white: '#ffffff',
+        black: '#000000',
+        darkblue: '#234568'
+      }
     },
   },
   plugins: [],
