32d659267819ccc16a9d8fa42af5ad524afbac0b
mihirniyogi <E1122052@u.nus.edu>
2024-02-20 19:51:19 +0800

    made it server-side rendering, cleaned up code

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,36 +1,13 @@
-'use client';
 import Header from '@/components/Header'
-import { poppins } from '../layout'
-import { usePathname } from 'next/navigation';
-import Link from 'next/link';
+import LSNavigation from '@/components/LSNavigation';
 
 const Login = () => {
-
-  const pathname = usePathname();
-
-  const navElements = [
-    {label: "Login", href: "/login"},
-    {label: "Signup", href: "/signup"},
-  ];
 
   return (
     <main>
 
       <Header title={"Log In Page"}>
-
-        <div className='absolute bottom-0 right-0 flex space-x-4 mr-4 mb-4'>
-        
-          {navElements.map(({label, href}) => (
-            <Link
-              key={label}
-              href={href}
-              className= {pathname === href ? "active text-white font-bold underline" : "text-white"}>
-              <p className={poppins.className}>{label}</p>
-            </Link>
-          ))}
-        
-        </div>
-
+        <LSNavigation currentPage='login'/>
       </Header> 
 
     </main>
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,36 +1,17 @@
-'use client';
 import Header from '@/components/Header'
-import { poppins } from '../layout'
-import { usePathname } from 'next/navigation';
-import Link from 'next/link';
+import LSNavigation from '@/components/LSNavigation';
 
-export default function Signup() {
-
-  const pathname = usePathname();
-
-  const navElements = [
-    {label: "Login", href: "/login"},
-    {label: "Signup", href: "/signup"},
-  ];
+const Signup = () => {
 
   return (
     <main>
-      <Header title={"Sign Up Page"}>
-      
-        <div className='absolute bottom-0 right-0 flex space-x-4 mr-4 mb-4'>
 
-        {navElements.map(({label, href}) => (
-            <Link
-              key={label}
-              href={href}
-              className= {pathname === href ? "active text-white font-bold underline" : "text-white"}>
-              <p className={poppins.className}>{label}</p>
-            </Link>
-          ))}
- 
-        </div>
-      
-      </Header>
+      <Header title={"Sign Up Page"}>
+        <LSNavigation currentPage='signup'/>
+      </Header> 
+
     </main>
   )
 }
+
+export default Signup;
